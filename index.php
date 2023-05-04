ff25f31fce9c64d7fbe7860f633b4186cb88303d
guy <guypeer1985@gmail.com>
2023-05-04 11:58:46 +0300

    change index.php

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
-<?php
-echo "trying again 2";
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My Webpage</title>
+	<style>
+		body {
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+			justify-content: center;
+			height: 100vh;
+			margin: 0;
+			padding: 0;
+			text-align: center;
+		}
+	</style>
+</head>
+<body>
+	<img src="594586957621731699536.jpg" alt="My Image" width="400" height="400">
+	<h1>Success!!!</h1>
+</body>
+</html>
